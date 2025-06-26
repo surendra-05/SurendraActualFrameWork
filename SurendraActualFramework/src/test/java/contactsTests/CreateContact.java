f8c28656d63a6dbe09b08126444ecec4e184c62a
Admin <Admin@DESKTOP-8GAR14I>
2025-06-25 19:41:09 -0700

    create contacts and organizations

---
+++ SurendraActualFramework/src/test/java/contactsTests/CreateContact.java
@@ -0,0 +1,10 @@
+package contactsTests;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SurendraActualFramework/src/test/java/orgnizationTests/CreateOrgTest.java
@@ -0,0 +1,10 @@
+package orgnizationTests;
+
+public class CreateOrgTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

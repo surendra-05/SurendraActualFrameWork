c0c8c9a591d60a3f044517ea3cda070961e8bc22
Jaga <Jaga@JP>
2025-06-26 16:12:40 +0530

    pushed

--- SurendraActualFramework/src/test/java/orgnizationTests/CreateOrgTest.java
+++ SurendraActualFramework/src/test/java/orgnizationTests/CreateOrgTest.java
@@ -3,7 +3,7 @@ package orgnizationTests;
 public class CreateOrgTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("organization created Successfully");
 
 	}
 
